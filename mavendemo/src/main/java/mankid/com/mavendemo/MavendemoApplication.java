df0df9ebb423d17138fc9edb1310251d20a00154
ue4-fans-code521 <kongziliang714@163.com>
2018-12-23 23:08:40 +0800

    后续添加的module01中的class(oppo)

---
+++ mavendemo/src/main/java/mankid/com/mavendemo/MavendemoApplication.java
@@ -0,0 +1,14 @@
+package mankid.com.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavendemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavendemoApplication.class, args);
+    }
+
+}
+
